98eb24f74804d7d75ff2030c89ae7b4776d42fd3
vites86@gmail.com <viktor.dubrovin@ukr.net>
2021-03-04 00:14:45 +0200

    Add timer.

--- msac-fighter/MainWindow.xaml.cs
+++ msac-fighter/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,17 +22,62 @@ namespace msac_fighter
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        public int timer_minutes;
+        public int timer_seconds;
+        public int timer_miliseconds;
+
         public MainWindow()
         {
             InitializeComponent();
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(1);
-            timer.Tick += Timer_Tick;
-            timer.Start();
+
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1);
+            dispatcherTimer.Tick += Timer_Tick;
         }
+
+        private void startbtn_Click(object sender, RoutedEventArgs e)
+        {
+            timer_minutes = string.IsNullOrEmpty(LblTimeMin.Text) ? 3 : Convert.ToInt32(LblTimeMin.Text);
+            timer_seconds = string.IsNullOrEmpty(LblTimeSec.Text) ? 0 : Convert.ToInt32(LblTimeSec.Text);
+            timer_miliseconds = string.IsNullOrEmpty(LblTimeMiliSec.Text) ? 00 : Convert.ToInt32(LblTimeMiliSec.Text);
+            dispatcherTimer.Start();
+        }
+
+        private void stopbtn_Click(object sender, RoutedEventArgs e)
+        {
+             dispatcherTimer.Stop();
+        }
+
+        private void resetbtn_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
         void Timer_Tick(object sender, EventArgs e)
         {
-            LblTime.Text = DateTime.Now.ToString("HH:mm:ss.fff");
+
+            timer_miliseconds--;
+
+            if (timer_miliseconds == -1)
+            {
+                timer_seconds--;
+                timer_miliseconds = 99;
+            }
+
+            if (timer_seconds==-1)
+            {
+                timer_minutes--;
+                timer_seconds = 59;
+            }
+
+            if (timer_seconds == 0 && timer_minutes==0 && timer_miliseconds == 0)
+            {
+                dispatcherTimer.Stop();
+            }
+
+            LblTimeMin.Text = timer_minutes.ToString("D2");
+            LblTimeSec.Text = timer_seconds.ToString("D2");
+            LblTimeMiliSec.Text = timer_miliseconds.ToString("D2");
+
         }
         private void RadioButtonMen_Clicked(object sender, RoutedEventArgs e)
         {
@@ -60,5 +106,7 @@ namespace msac_fighter
                 ButtonTo.FontWeight = FontWeights.Bold;
             }
         }
+
+       
     }
 }
